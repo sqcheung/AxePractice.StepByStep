c5b251b8d71877cd06cf907a8c53ef9bbc6f8e05
sqzhang <sqzhang@thoughtworks.com>
2017-08-17 11:44:31 +0800

    [shengqi] request accept type negotiate response content type.

--- src/WebApi/practices/04_http_message_handling/src/SampleWebApi/MessageController.cs
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/MessageController.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Net;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace SampleWebApi
@@ -14,9 +15,29 @@ namespace SampleWebApi
             // order to pass the test.
             // You can add new files if you want. But you cannot change any existed code.
 
-            throw new NotImplementedException();
+            IContentNegotiator negotiator = this.Configuration.Services.GetContentNegotiator();
+
+            ContentNegotiationResult result = negotiator.Negotiate( typeof(MessageObject), this.Request, this.Configuration.Formatters);
+            if (result == null)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+            return new HttpResponseMessage()
+            {
+                Content = new ObjectContent<MessageObject>(
+                    new MessageObject() {Message = "Hello"},                // What we are serializing 
+                    result.Formatter,           // The media formatter
+                    result.MediaType.MediaType  // The MIME type
+                )
+            };
+            
 
             #endregion
         }
+    }
+
+    public class MessageObject
+    {
+        public string Message { get; set; }
     }
 }
