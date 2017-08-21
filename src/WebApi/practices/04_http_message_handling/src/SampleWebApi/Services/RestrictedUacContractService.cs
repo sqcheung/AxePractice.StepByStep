4170e922f977b601d4ce495ed0dfeb827a16ada9
sqzhang <sqzhang@thoughtworks.com>
2017-08-21 22:27:22 +0800

    [shengqi] reimplement RemoveRestrictedInfo.

--- src/WebApi/practices/04_http_message_handling/src/SampleWebApi/Services/RestrictedUacContractService.cs
+++ src/WebApi/practices/04_http_message_handling/src/SampleWebApi/Services/RestrictedUacContractService.cs
@@ -1,4 +1,7 @@
-﻿using SampleWebApi.Repositories;
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+using SampleWebApi.DomainModel;
+using SampleWebApi.Repositories;
 
 namespace SampleWebApi.Services
 {
@@ -11,5 +14,38 @@ namespace SampleWebApi.Services
             this.roleRepository = roleRepository;
         }
 
+        public bool RemoveRestrictedInfo(long userId, JObject restricetedResource)
+        {
+            if (roleRepository.Get(userId) == Role.Admin) { return false; }
+
+            var Links = restricetedResource["links"] as JArray;
+            if (Links == null) { return false; }
+            JToken[] restrictedLinks = Links.Where(link =>
+            {
+                var jObjectLink = link as JObject;
+                if (jObjectLink == null)
+                {
+                    return false;
+                }
+                JToken jTokenLink = jObjectLink?["restricted"];
+                if (jTokenLink?.Type != JTokenType.Boolean)
+                {
+                    return false;
+                }
+
+                return jTokenLink.Value<bool>();
+            }).ToArray();
+
+            if (restrictedLinks.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (JToken restrictedLink in restrictedLinks)
+            {
+                Links.Remove(restrictedLink);
+            }
+            return true;
+        }
     }
 }
